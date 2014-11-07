fa546f8d4f5ac04a108f474e21f1060261bc7b77
Salvoxia <Salvoxia@blindfish.info>
2014-11-07 18:17:02 +0100

    Increased settings input field size
    Increased the settings input field size to fully accomodate the client ID
    and client secret generated by CCP. Pure cosmetic and usability change.

--- eve_sso/settings.php
+++ eve_sso/settings.php
@@ -16,8 +16,8 @@ $html .= $confirm;
 $html .='<form action="" method="post">';
 $html .= "<table class=kb-subtable>";
 $html .= "<tr><td colspan=\"4\"><div class=block-header2>SSO options</div></td></tr>";
-$html .= "<tr><td><b>EVE SSO Client id:</b></td><td><input type='text' name='client_id' value='".config::get('eve_sso_client_id')."'/></td></tr>";
-$html .= "<tr><td><b>EVE SSO Client secret:</b></td><td><input type='text' name='secret' value='".config::get('eve_sso_secret')."'/></td></tr>";
+$html .= "<tr><td><b>EVE SSO Client id:</b></td><td><input type='text' size='45' name='client_id' value='".config::get('eve_sso_client_id')."'/></td></tr>";
+$html .= "<tr><td><b>EVE SSO Client secret:</b></td><td><input type='text' size='45' name='secret' value='".config::get('eve_sso_secret')."'/></td></tr>";
 
 $html .= "<tr><td><b>Only allow killboard corp/alliance members to post:</b></td><td><input type='checkbox' name='owners_only'";
 if (config::get('eve_sso_owners_only'))
